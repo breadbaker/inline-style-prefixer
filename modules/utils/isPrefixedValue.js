21f222a7de3cbeae352c9b82dc532c14dd659f6c
Dan Baker <dbaker@Daniels-MacBook-Pro-2.local>
2016-11-21 23:12:31 -0500

    actual fix

--- modules/utils/isPrefixedValue.js
+++ modules/utils/isPrefixedValue.js
@@ -1,5 +1,5 @@
 export default value => {
   if (Array.isArray(value)) value = value.join(',')
 
-  return value.match(/-webkit-|-moz-|-ms-/) !== null
+  return value.match(/-webkit-|-ms-/) !== null
 }
